7906006754c4dd8fb9b0a94c473b70c93bf82ab6
sarkar-abhranshu <abhranshusarkar@outlook.com>
2024-11-16 13:05:57 +0530

    Made change in server port for nodeJS server to make website fully functional alongwith database connection

--- charity-connect/server.js
+++ charity-connect/server.js
@@ -53,7 +53,7 @@ app.get('*', (req, res) => {
 });
 
 // Start server
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });

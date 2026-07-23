00bec4067e13f20d96e5306271ad0c347b8146fc
sarkar-abhranshu <abhranshusarkar@outlook.com>
2026-07-23 21:09:14 +0530

    fix: Delete backend as all logic now in frontend api route files itself

--- backend/sql/users.sql
+++
@@ -1,7 +0,0 @@
-create table if not exists users(
-    id serial primary key,
-    username varchar(50) unique not null,
-    password varchar(255) not null,
-    email varchar(100) unique not null,
-    role varchar(50) not null
-);
